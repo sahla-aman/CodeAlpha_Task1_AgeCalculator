3ce86450aa1e357a89f2d99c5efe6c94aa4fb379
Fathima Sahla <134530857+sahla-aman@users.noreply.github.com>
2024-03-10 21:47:16 +0530

    script.js

--- Task_1/script.js
+++ Task_1/script.js
@@ -1,27 +1,23 @@
 function calculateAge() {
-    var dob = document.getElementById('dob').value;
-    var mob = document.getElementById('mob').value;
-    var yob = document.getElementById('yob').value;
-  
-    var birthDate = new Date(yob, mob - 1, dob);
-    var currentDate = new Date();
-  
-    var years = currentDate.getFullYear() - birthDate.getFullYear();
-    var months = currentDate.getMonth() - birthDate.getMonth();
-    var days = currentDate.getDate() - birthDate.getDate();
-  
-    if (months < 0 || (months === 0 && days < 0)) {
-      years--;
-      months += 12;
-    }
-  
-    if (days < 0) {
-      var lastMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 0);
-      days += lastMonth.getDate();
-      months--;
-    }
-  
-    var result = document.getElementById('result');
-    result.textContent = 'Your age is: ' + years + ' years, ' + months + ' months, and ' + days + ' days.';
+  var dobInput = document.getElementById('dob');
+  var dob = new Date(dobInput.value);
+  var currentDate = new Date();
+
+  var years = currentDate.getFullYear() - dob.getFullYear();
+  var months = currentDate.getMonth() - dob.getMonth();
+  var days = currentDate.getDate() - dob.getDate();
+
+  if (months < 0 || (months === 0 && days < 0)) {
+    years--;
+    months += 12;
   }
-  
+
+  if (days < 0) {
+    var lastMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 0);
+    days += lastMonth.getDate();
+    months--;
+  }
+
+  var result = document.getElementById('result');
+  result.textContent = 'Your age is: ' + years + ' years, ' + months + ' months, and ' + days + ' days.';
+}
